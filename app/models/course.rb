ee552034a861d53c73b7276fb8ad31ff13d7c13b
Ndongala Garcia <garciafreelancer@gmail.com>
2018-12-05 04:08:06 +0100

    Add helpers

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ApplicationRecord
   belongs_to :category
   has_many :classcourses
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/curso-redes.jpg"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/curso-redes.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
